471c5827010f9ea1fbf088febd2b978b36eabadd
jacob <jmynerms@outlook.com>
2019-08-07 16:27:09 +0100

    Working clocks

--- README.markdown
+++ README.markdown
@@ -1,4 +1,4 @@
 # Digital Clock with Vue.js
- _A Pen created at CodePen.io. Original URL: [https://codepen.io/gau/pen/LjQwGp](https://codepen.io/gau/pen/LjQwGp).
+ _A Pen created at CodePen.io. Original URL: [https://codepen.io/ReNcH/pen/KOQEKP](https://codepen.io/ReNcH/pen/KOQEKP).
 
  
--- src/script.js
+++ src/script.js
@@ -6,6 +6,46 @@ var clock = new Vue({
     }
 });
 
+var clock2 = new Vue({
+    el: '#clock2',
+    data: {
+        time: '',
+        date: ''
+    }
+});
+
+var clock3 = new Vue({
+    el: '#clock3',
+    data: {
+        time: '',
+        date: ''
+    }
+});
+
+var clock4 = new Vue({
+    el: '#clock4',
+    data: {
+        time: '',
+        date: ''
+    }
+});
+
+var clock5 = new Vue({
+    el: '#clock5',
+    data: {
+        time: '',
+        date: ''
+    }
+});
+
+var clock6 = new Vue({
+    el: '#clock6',
+    data: {
+        time: '',
+        date: ''
+    }
+});
+
 var week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 var timerID = setInterval(updateTime, 1000);
 updateTime();
@@ -13,7 +53,24 @@ function updateTime() {
     var cd = new Date();
     clock.time = zeroPadding(cd.getHours(), 2) + ':' + zeroPadding(cd.getMinutes(), 2) + ':' + zeroPadding(cd.getSeconds(), 2);
     clock.date = zeroPadding(cd.getFullYear(), 4) + '-' + zeroPadding(cd.getMonth()+1, 2) + '-' + zeroPadding(cd.getDate(), 2) + ' ' + week[cd.getDay()];
+    
+    clock2.time = zeroPadding(cd.getHours() - 5, 2) + ':' + zeroPadding(cd.getMinutes(), 2) + ':' + zeroPadding(cd.getSeconds(), 2);
+    clock2.date = zeroPadding(cd.getFullYear(), 4) + '-' + zeroPadding(cd.getMonth()+1, 2) + '-' + zeroPadding(cd.getDate(), 2) + ' ' + week[cd.getDay()];
+    
+    clock3.time = zeroPadding(cd.getHours() + 8, 2) + ':' + zeroPadding(cd.getMinutes(), 2) + ':' + zeroPadding(cd.getSeconds(), 2);
+    clock3.date = zeroPadding(cd.getFullYear(), 4) + '-' + zeroPadding(cd.getMonth()+1, 2) + '-' + zeroPadding(cd.getDate(), 2) + ' ' + week[cd.getDay()];
+    
+    clock4.time = zeroPadding(cd.getHours() + 3, 2) + ':' + zeroPadding(cd.getMinutes(), 2) + ':' + zeroPadding(cd.getSeconds(), 2);
+    clock4.date = zeroPadding(cd.getFullYear(), 4) + '-' + zeroPadding(cd.getMonth()+1, 2) + '-' + zeroPadding(cd.getDate(), 2) + ' ' + week[cd.getDay()];
+    
+    clock5.time = zeroPadding(cd.getHours() - 8, 2) + ':' + zeroPadding(cd.getMinutes(), 2) + ':' + zeroPadding(cd.getSeconds(), 2);
+    clock5.date = zeroPadding(cd.getFullYear(), 4) + '-' + zeroPadding(cd.getMonth()+1, 2) + '-' + zeroPadding(cd.getDate(), 2) + ' ' + week[cd.getDay()];
+    
+    clock6.time = zeroPadding(cd.getHours() + 5 , 2) + ':' + zeroPadding(cd.getMinutes() + 30, 2) + ':' + zeroPadding(cd.getSeconds(), 2);
+    clock6.date = zeroPadding(cd.getFullYear(), 4) + '-' + zeroPadding(cd.getMonth()+1, 2) + '-' + zeroPadding(cd.getDate(), 2) + ' ' + week[cd.getDay()];
 };
+
+
 
 function zeroPadding(num, digit) {
     var zero = '';
